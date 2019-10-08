92a58be08aa8ca0dd9b832e0f7e15002339f938a
Kawa-oneechan <kawaoneechan@gmail.com>
2019-10-08 17:56:14 +0200

    Slideshow: found out why the pics broke
    It was interrupts.

--- slideshow/main.c
+++ slideshow/main.c
@@ -5,31 +5,34 @@ void* LoadFile(const char* path, void* buffer, int32_t len)
 {
 	FILE file;
 	FILEINFO nfo;
+	int regs = REG_INTRMODE;
+	REG_INTRMODE |= 0x80;
 	int32_t ret = DISK->FileStat(path, &nfo);
-	//printf("%i, %i -> %i\n", ret, nfo.fsize, len);
-	//void* mem = malloc(nfo.fsize);
-	//printf("%i\n", mem);
-	//return 0;
-	//if (mem == 0) return 0;
+
 	ret = DISK->OpenFile(&file, path, FA_READ);
-	//void* target = mem;
-	if (ret > 0) return (void*)ret;
+	if (ret > 0)
+	{
+		REG_INTRMODE = regs;
+		return (void*)ret;
+	}
 
 	void *target = buffer;
 	for(;;)
 	{
 		ret = DISK->ReadFile(&file, target, 1024);
-		if (ret < 0) return (void*)ret;
+		if (ret < 0)
+		{
+			REG_INTRMODE = regs;
+			return (void*)ret;
+		}
+
 		target += ret;
 		if (ret < 1024)
 			break;
 	}
 
-	//if (nfo.fsize < len) len = nfo.fsize;
-	//printf("LoadFile: gonna try reading %d bytes to %x.\n", len, buffer);
-	//ret = DISK->ReadFile(&file, buffer, len);
-	//if (ret < 0) return (void*)ret;
 	DISK->CloseFile(&file);
+	REG_INTRMODE = regs;
 	return buffer;
 }
 
@@ -46,34 +49,28 @@ int32_t main(void)
 	int ret;
 	DIR dir;
 	FILEINFO info;
+	REG_INTRMODE |= 0x80;
+
 	TImageFile* image = NULL;
 	ret = DISK->FindFirst(&dir, &info, "0:", "*.api");
+
 	while(ret == 0 && info.fname[0])
 	{
-		//dpf("%s\n", info.fname);
 		image = malloc(info.fsize);
 		if (image == NULL)
-		{
 			continue;
-		}
+
 		LoadFile((const char*)info.fname, (void*)image, info.fsize);
 		if (image->BitDepth != 4 && image->BitDepth != 8)
-		{
-			free(image);
 			continue;
-		}
-		//MISC->DmaClear((void*)0x0E000000, 0, 640 * 480, DMA_SHORT);
 		DRAW->DisplayPicture(image);
 		//PALETTE[15] = 0x7FFF;
 		//DRAW->DrawString(info.fname, 9, 9, 0);
 		//DRAW->DrawString(info.fname, 8, 8, 15);
 		DRAW->FadeFromBlack();
 		free(image);
-
-		//MISC->WaitForVBlanks(60);
 		WaitForKey();
 		DRAW->FadeToBlack();
-
 		ret = DISK->FindNext(&dir, &info);
 	}
 	MISC->SetTextMode(SMODE_240 | SMODE_BOLD);
