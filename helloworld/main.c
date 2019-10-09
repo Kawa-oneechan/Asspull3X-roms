1038c3a31fc5b4551e5e9b6f9bd1d79c811309f3
Kawa-oneechan <kawaoneechan@gmail.com>
2019-10-09 11:35:10 +0200

    HelloWorld: clean up more

--- helloworld/main.c
+++ helloworld/main.c
@@ -1,19 +1,11 @@
 #include "../ass.h"
 IBios* interface;
 
-void WaitForKey()
-{
-	while (REG_KEYIN == 0);
-	vbl();
-	while (REG_KEYIN != 0);
-}
-
 int main(void)
 {
 	interface = (IBios*)(0x01000000);
 	MISC->SetTextMode(SMODE_240 | SMODE_BOLD);
 	TEXT->SetTextColor(1, 7);
 	TEXT->ClearScreen();
-	TEXT->SetCursorPosition(0, 0);
 	printf("Hello, world!\n");
 }
