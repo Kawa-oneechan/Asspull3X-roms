c0906de8cf0b9981b0f05936fa78f228a3904d25
Kawa <kawaoneechan@gmail.com>
2021-08-12 13:47:54 +0200

    Test: mildly improve bitmap test

--- test/bitmaps.c
+++ test/bitmaps.c
@@ -7,9 +7,15 @@ extern const TImageFile bmp640x480x4;
 extern const TImageFile bmp320x200x8;
 extern const TImageFile bmp320x240x8;
 extern const TImageFile bmp640x480x8;
-
-void WaitForKeySilent()
+const TImageFile * const bitmaps[] =
 {
+	&bmp320x200x4, &bmp320x240x4, &bmp640x480x4,
+	&bmp320x200x8, &bmp320x240x8, &bmp640x480x8,
+};
+
+void WaitForKey2()
+{
+	DRAW->DrawString("Press any key to continue.", 0, 0, 15);
 	while (REG_KEYIN != 0) { vbl(); }
 	while (REG_KEYIN == 0) { vbl(); }
 	while (REG_KEYIN != 0) { vbl(); }
@@ -17,17 +23,11 @@ void WaitForKeySilent()
 
 void BitmapTest()
 {
-	DRAW->DisplayPicture((TImageFile*)&bmp320x200x4);
-	WaitForKeySilent();
-	DRAW->DisplayPicture((TImageFile*)&bmp320x240x4);
-	WaitForKeySilent();
-	DRAW->DisplayPicture((TImageFile*)&bmp640x480x4);
-	WaitForKeySilent();
-	DRAW->DisplayPicture((TImageFile*)&bmp320x200x8);
-	WaitForKeySilent();
-	DRAW->DisplayPicture((TImageFile*)&bmp320x240x8);
-	WaitForKeySilent();
-	DRAW->DisplayPicture((TImageFile*)&bmp640x480x8);
-	WaitForKeySilent();
-//	DRAW->ResetPalette();
+	interface->DrawCharFont -= 0x800;
+	for (int i = 0; i < 6; i++)
+	{
+		DRAW->DisplayPicture((TImageFile*)bitmaps[i]);
+		WaitForKey2();
+	}
+	DRAW->ResetPalette();
 }
