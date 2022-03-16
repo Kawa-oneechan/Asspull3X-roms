564f4eae9e885ab107552cc7fc6da4c4f3f260f3
Kawa <kawaoneechan@gmail.com>
2022-03-17 00:02:32 +0100

    Tiletest: use proper SMB1 tileset rip
    meh why not
    
    also, add a status line

--- tiletest/main.c
+++ tiletest/main.c
@@ -6,7 +6,7 @@ IBios* interface;
 
 extern const uint16_t tilesetPal[], tilesetTiles[], hdma1[];
 extern const uint16_t farahPal[], farahTiles[];
-extern const uint16_t grassTiles[], questionTiles[];
+extern const uint16_t tileanimTiles[];
 extern const uint16_t bg1Map[], bg2Map[], levelMap[];
 
 void DrawStripe(int source, int target)
@@ -19,9 +19,21 @@ void DrawStripe(int source, int target)
 	uint16_t *d3 = &(MAP3[target]);
 	for (int i = 0; i < 34; i++)
 	{
-		*d1 = *s1;
-		*d2 = *s2;
-		*d3 = *s3;
+		if (*s1 > 256)
+			*d1 = *s1;
+		else
+			*d1 = 0;
+		if (*s2 > 256)
+			*d2 = *s2;
+		else
+			*d2 = 0;
+		if (i > 6)
+		{
+			if (*s3 > 256)
+				*d3 = *s3;
+			else
+				*d3 = 0;
+		}
 		s1 += MAPWIDTH;
 		s2 += MAPWIDTH;
 		s3 += MAPWIDTH;
@@ -35,7 +47,7 @@ typedef struct
 {
 	uint16_t palette:4;
 	uint16_t enabled:1;
-	uint16_t _waste:2;
+	uint16_t blend:2;
 	uint16_t tile:9;
 } TObjectA;
 #define objectsA ((TObjectA*)OBJECTS_A)
@@ -77,13 +89,13 @@ int main(void)
 	REG_HDMATARGET[0] = (int32_t)PALETTE;
 	REG_HDMACONTROL[0] = DMA_ENABLE | HDMA_DOUBLE | (DMA_SHORT << 4) | (0 << 8) | (480 << 20);
 
-	MISC->DmaCopy(TILESET, (int8_t*)&tilesetTiles, 2560, DMA_INT);
-	MISC->DmaCopy(PALETTE, (int8_t*)&tilesetPal, 32, DMA_SHORT);
+	MISC->DmaCopy(TILESET + (32*256), (int8_t*)&tilesetTiles, 20480 / 4, DMA_INT);
+	MISC->DmaCopy(PALETTE, (int8_t*)&tilesetPal, 128, DMA_SHORT);
 
-	MISC->DmaCopy(TILESET + 0x2000, (int8_t*)&farahTiles, 64, DMA_INT);
+	MISC->DmaCopy(TILESET + 32, (int8_t*)&farahTiles, 64, DMA_INT);
 	MISC->DmaCopy(PALETTE + 256, (int8_t*)&farahPal, 16, DMA_SHORT);
 
-	objectsA[0].tile = 256;
+	objectsA[0].tile = 1;
 	objectsA[0].enabled = 1;
 	objectsB[0].x = 152;
 	objectsB[0].y = 176;
@@ -91,21 +103,43 @@ int main(void)
 	objectsB[0].large = 1;
 
 	REG_MAPSET = 0x70; //just enable it, don't worry about tile offsets.
+	//REG_MAPBLEND = 0x01;
 
 	for (int i = 0; i < SCREENWIDTH; i++)
 		DrawStripe(i, i);
 
+	{
+		const char buffer[] = "TILE TEST - V2";
+		char *c = (char*)buffer;
+		int pos = 65;
+		while (*c)
+		{
+			if (*c >= '0' && *c <= '9')
+				MAP3[pos] = *c - '0' + 256;
+			else if (*c >= 'A' && *c <= 'Z')
+				MAP3[pos] = *c - 'A' + 266;
+			else if (*c == '-')
+				MAP3[pos] = 296;
+			c++;
+			pos++;
+		}
+	}
+
 	inton();
 
-	REG_SCROLLY1 = 32;
-	REG_SCROLLY2 = 32;
-	REG_SCROLLY3 = 16;
+	REG_SCROLLY1 = 0;
+	REG_SCROLLY2 = 0;
+	REG_SCROLLY3 = 0;
 	int scroll = 0;
 	int col = 40;
 	int animation = 0;
 	for(;;)
 	{
 		vbl();
+
+		//SMB1
+		REG_SCROLLX3 = 0;
+		while (REG_LINE < 32);
 
 		//Something to test the object structs with...
 		REG_JOYPAD = 1;
@@ -122,13 +156,16 @@ int main(void)
 		if (REG_TICKCOUNT % 4 == 0)
 		{
 			animation++;
-			MISC->DmaCopy(TILESET + 0x020, (int8_t*)&questionTiles + ((animation % 9) * 128), 32, DMA_INT);
-			MISC->DmaCopy(TILESET + 0x240, (int8_t*)&grassTiles + ((animation % 3) * 192), 48, DMA_INT);
+			MISC->DmaCopy(TILESET + (32*0x1C0), (int8_t*)&tileanimTiles + ((animation % 8) * (32*4*16)), (8*4*16), DMA_INT);
 		}
 		if (scroll % 8 == 0)
 		{
 			col++;
 			DrawStripe(col % MAPWIDTH, col % SCREENWIDTH);
 		}
+
+		//SMB3
+		//while (REG_LINE < 400);
+		//REG_SCROLLX3 = REG_SCROLLY3 = 0;
 	}
 }
