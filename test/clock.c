b562714eb3c4b96e7eae8228a6a41b668a2cf65c
Kawa <kawaoneechan@gmail.com>
2022-04-19 22:26:54 +0200

    Test: add an analog clock for no reason

--- test/clock.c
+++ test/clock.c
@@ -40,6 +40,61 @@ void DrawClock(tm* now_tm)
 	printf("\x8C\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x8D\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x92");
 }
 
+
+const int FontWH_Ratio = 2;
+const int cosa[] = { 0, 105, 208, 309, 407, 500, 588, 669, 743, 809, 866, 914, 951, 978, 995, 1000, 995, 978, 951, 914, 866, 809, 743, 669, 588, 500, 407, 309, 208, 105, 0, -105, -208, -309, -407, -500, -588, -669, -743, -809, -866, -914, -951, -978, -995, -1000, -995, -978, -951, -914, -866, -809, -743, -669, -588, -500, -407, -309, -208, -105 };
+const int sina[] = { -1000, -995, -978, -951, -914, -866, -809, -743, -669, -588, -500, -407, -309, -208, -105, 0, 105, 208, 309, 407, 500, 588, 669, 743, 809, 866, 914, 951, 978, 995, 1000, 995, 978, 951, 914, 866, 809, 743, 669, 588, 500, 407, 309, 208, 105, 0, -105, -208, -309, -407, -500, -588, -669, -743, -809, -866, -914, -951, -978, -995 };
+
+static void drawChar(int x, int y, int c)
+{
+	TEXTMAP[(y * 80) + x] = c;
+}
+
+static void drawHand(int minute, int hlength, int c, int sYcen, int sXcen)
+{
+	int x, y, n;
+	for (n = 1; n < hlength; n++)
+	{
+		x = (cosa[minute] * n * FontWH_Ratio / 1000) + sXcen;
+		y = (sina[minute] * n / 1000) + sYcen;
+		drawChar(x, y, c);
+	}
+}
+
+static void drawCircle(int hand_max, int sYcen, int sXcen)
+{
+	int x, y, r;
+	int c;
+
+	for (r = 0; r < 60; r++)
+	{
+		x = (cosa[r] * hand_max * FontWH_Ratio / 1000) + sXcen;
+		y = (sina[r] * hand_max / 1000) + sYcen;
+		switch (r)
+		{
+			case 0:
+			case 5:
+			case 10:
+			case 15:
+			case 20:
+			case 25:
+			case 30:
+			case 35:
+			case 40:
+			case 45:
+			case 50:
+			case 55:
+				c = 0x880F;
+				break;
+			default:
+				c = 0xB807;
+				break;
+		}
+		drawChar(x, y, c);
+	}
+}
+
+
 void ClockTest()
 {
 reset:
@@ -194,10 +249,27 @@ reset:
 
 	REG_CARET = 0;
 
+	const int hand_max = 10;
+	const int sXcen = 50;
+	const int sYcen = 16;
+
+	drawCircle(hand_max, sYcen, sXcen);
+
 	while(INP_KEYIN == 0)
 	{
+		if (now_t != REG_TIMET)
+		{
+			drawHand(now_tm->tm_hour % 12 * 5 + now_tm->tm_min / 12, 2 * hand_max / 3, 0, sYcen, sXcen);
+			drawHand(now_tm->tm_min, hand_max - 2, 0, sYcen, sXcen);
+			drawHand(now_tm->tm_sec, hand_max - 1, 0, sYcen, sXcen);
+		}
+
 		now_t = REG_TIMET;
 		now_tm = gmtime(&now_t);
+
+		drawHand(now_tm->tm_hour % 12 * 5 + now_tm->tm_min / 12, hand_max - 3, 0x0809, sYcen, sXcen);
+		drawHand(now_tm->tm_min, hand_max - 2, 0x080A, sYcen, sXcen);
+		drawHand(now_tm->tm_sec, hand_max - 1, 0x080B, sYcen, sXcen);
 
 		DrawClock(now_tm);
 
