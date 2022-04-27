99ed489de0bccc49f66f0b87c9cf19c71278cdc1
Kawa <kawaoneechan@gmail.com>
2022-04-27 22:37:05 +0200

    Test: add more buttons

--- test/joypad.c
+++ test/joypad.c
@@ -23,40 +23,62 @@ extern const uint16_t pointerTiles[], pointerPal[];
 
 void JoypadTest()
 {
-	MISC->SetTextMode(SMODE_240 | SMODE_BOLD);
-	TEXT->SetTextColor(0, 7);
-	TEXT->ClearScreen();
+	//MISC->SetTextMode(SMODE_240 | SMODE_BOLD);
+	//TEXT->SetTextColor(0, 7);
+	//TEXT->ClearScreen();
+	MISC->SetBitmapMode16(SMODE_320 | SMODE_240);
+	MISC->DmaClear(BITMAP, 0, 160 * 240, DMA_BYTE);
+
 	MISC->DmaCopy(TILESET + 0x2000, (int8_t*)&pointerTiles, 0x400, DMA_SHORT);
 	MISC->DmaCopy(PALETTE + 256, pointerPal, 16, DMA_SHORT);
 	OBJECTS_A[0] = OBJECTA_BUILD(260, 0, 1, 0);
 	OBJECTS_B[0] = OBJECTB_BUILD(320, 160, 0, 0, 0, 0, 1, 0);
 
-	OBJECTS_B[1] = OBJECTB_BUILD(216 + 16, 32, 0, 0, 0, 0, 1, 0);
-	OBJECTS_B[2] = OBJECTB_BUILD(216 + 32, 24, 0, 0, 0, 0, 1, 0);
-	OBJECTS_B[3] = OBJECTB_BUILD(216 +  0, 24, 0, 0, 0, 0, 1, 0);
-	OBJECTS_B[4] = OBJECTB_BUILD(216 + 16, 16, 0, 0, 0, 0, 1, 0);
+	OBJECTS_B[1] = OBJECTB_BUILD(200, 104, 0, 0, 0, 0, 1, 0);
+	OBJECTS_B[2] = OBJECTB_BUILD(216,  88, 0, 0, 0, 0, 1, 0);
+	OBJECTS_B[3] = OBJECTB_BUILD(184,  88, 0, 0, 0, 0, 1, 0);
+	OBJECTS_B[4] = OBJECTB_BUILD(200,  72, 0, 0, 0, 0, 1, 0);
 
-	OBJECTS_B[5] = OBJECTB_BUILD(160 + 16, 32, 0, 0, 0, 0, 1, 0);
-	OBJECTS_B[6] = OBJECTB_BUILD(160 + 32, 24, 0, 0, 0, 0, 1, 0);
-	OBJECTS_B[7] = OBJECTB_BUILD(160 +  0, 24, 0, 0, 0, 0, 1, 0);
-	OBJECTS_B[8] = OBJECTB_BUILD(160 + 16, 16, 0, 0, 0, 0, 1, 0);
+	OBJECTS_B[5] = OBJECTB_BUILD(104, 104, 0, 0, 0, 0, 1, 0);
+	OBJECTS_B[6] = OBJECTB_BUILD(120,  88, 0, 0, 0, 0, 1, 0);
+	OBJECTS_B[7] = OBJECTB_BUILD( 88,  88, 0, 0, 0, 0, 1, 0);
+	OBJECTS_B[8] = OBJECTB_BUILD(104,  72, 0, 0, 0, 0, 1, 0);
 
-	//OBJECTS_B[5] = OBJECTB_BUILD(160 + (i * 18), 24, 0, 0, 0, 0, 1, 0);
+	OBJECTS_B[9] = OBJECTB_BUILD( 96,  48, 0, 0, 0, 0, 1, 0);
+	OBJECTS_B[10]= OBJECTB_BUILD(208,  48, 0, 0, 0, 0, 1, 0);
+	OBJECTS_B[11]= OBJECTB_BUILD(144,  80, 0, 0, 0, 0, 1, 0);
+	OBJECTS_B[12]= OBJECTB_BUILD(160,  80, 0, 0, 0, 0, 1, 0);
+
+	OBJECTS_A[13]= OBJECTA_BUILD(312,  9, 1, 0);
+	OBJECTS_A[14]= OBJECTA_BUILD(316,  9, 1, 0);
+	OBJECTS_B[13]= OBJECTB_BUILD(144,  96, 0, 0, 0, 0, 1, 0);
+	OBJECTS_B[14]= OBJECTB_BUILD(160,  96, 0, 0, 0, 0, 1, 0);
 
 	const char * const types[] = { "none", "digital only", "analog" };
-	printf("1. %s\n", types[INP_JOYSTATES & 0x0F]);
-	printf("2. %s\n", types[(INP_JOYSTATES >> 4) & 0x0F]);
+	char buffer[256] = { 0 };
+	sprintf(buffer, "1. %s\n2. %s", types[INP_JOYSTATES & 0x0F], types[(INP_JOYSTATES >> 4) & 0x0F]);
+	DRAW->DrawString(buffer, 0, 0, 15);
 
+	int oldButtons = -1;
+	char oldAxis[2] = { -1, -1 };
 	while (1)
 	{
 		int buttons = INP_JOYPAD1;
 		char axis1 = INP_JOYSTK1H;
 		char axis2 = INP_JOYSTK1V;
 
-		TEXT->SetCursorPosition(0, 3);
-		printf("0x%04X %3d,%3d  ", buttons, axis1, axis2);
+		//TEXT->SetCursorPosition(0, 3);
+		if (buttons != oldButtons || axis1 != oldAxis[0] || axis2 != oldAxis[1])
+		{
+			MISC->DmaClear(BITMAP + (160 * 24), 0, 160 * 8, DMA_BYTE);
+			sprintf(buffer, "0x%04X %3d,%3d  ", buttons, axis1, axis2);
+			DRAW->DrawString(buffer, 0, 24, 15);
+			oldButtons = buttons;
+			oldAxis[0] = axis1;
+			oldAxis[1] = axis2;
+		}
 
-		OBJECTS_B[0] = OBJECTB_BUILD(320 + (axis1 / 1) - 8, 120 + (axis2 / 2) - 8, 0, 0, 0, 0, 1, 0);
+		OBJECTS_B[0] = OBJECTB_BUILD(160 + (axis1 / 2) - 8, 120 + (axis2 / 2) - 8, 0, 0, 0, 0, 1, 0);
 
 		OBJECTS_A[1] = OBJECTA_BUILD(264, (buttons & 0x10) ? 0 : 1, 1, 0);
 		OBJECTS_A[2] = OBJECTA_BUILD(268, (buttons & 0x20) ? 0 : 1, 1, 0);
@@ -66,6 +88,11 @@ void JoypadTest()
 		OBJECTS_A[6] = OBJECTA_BUILD(284, (buttons & 0x02) ? 0 : 1, 1, 0);
 		OBJECTS_A[5] = OBJECTA_BUILD(288, (buttons & 0x04) ? 0 : 1, 1, 0);
 		OBJECTS_A[7] = OBJECTA_BUILD(292, (buttons & 0x08) ? 0 : 1, 1, 0);
+
+		OBJECTS_A[9] = OBJECTA_BUILD(296, (buttons & 0x100) ? 0 : 1, 1, 0);
+		OBJECTS_A[10]= OBJECTA_BUILD(300, (buttons & 0x200) ? 0 : 1, 1, 0);
+		OBJECTS_A[11]= OBJECTA_BUILD(304, (buttons & 0x400) ? 0 : 1, 1, 0);
+		OBJECTS_A[12]= OBJECTA_BUILD(308, (buttons & 0x800) ? 0 : 1, 1, 0);
 
 		vbl();
 	}
