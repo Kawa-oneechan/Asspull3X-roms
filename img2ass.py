83bf9c3b6fe55920aed4abf43263a48866174b22
Kawa <kawaoneechan@gmail.com>
2022-07-25 14:05:27 +0200

    Update img2ass.py

--- img2ass.py
+++ img2ass.py
@@ -9,6 +9,8 @@ parser.add_argument('inFile', help='source image file')
 parser.add_argument('outFile', nargs='?', help='target .api file')
 parser.add_argument('-r', '--raw', help='skip compression', action='store_true')
 parser.add_argument('-v', '--verbose', help='use verbose output', action='store_true')
+parser.add_argument('-g', '--nograds', help='skip HDMA gradients', action='store_true')
+parser.add_argument('-c', '--clipgrads', help='clip HDMA gradients', action='store_true')
 args = parser.parse_args()
 
 stem = Path(args.inFile).stem
@@ -39,12 +41,15 @@ if fourBits:
 
 palSize = 32 if fourBits else 512
 palLength = 16 if fourBits else 256
+palOffset = 0x18
+dataOffset = palOffset + palSize
 
 size = im.width * im.height
 if not fourBits:
 	size /= 2
 
 compressed = True
+depth = 4 if fourBits else 8
 
 if fourBits:
 	newData = bytearray()
@@ -97,25 +102,51 @@ else:
 	else:
 		compressed = False
 
+dataSize = len(outData)
+dataOffset = palOffset + palSize
+
+flags = 1 if compressed else 0
+
+hdmaChannels = []
+for i in range(0, 8):
+	hdmaFile = stem + f'-h{i}.png'
+	if os.path.exists(hdmaFile):
+		hdmaChannels.append(i)
+if args.nograds:
+	if len(hdmaChannels):
+		print(f'Ignoring {len(hdmaChannels)} HDMA channel(s).')
+	hdmaChannels = []
+if len(hdmaChannels) > 0:
+	flags = flags | 2
+	palOffset = palOffset + 4
+	dataOffset = dataOffset + 4
+hdmaOffset = dataOffset + dataSize
+
 if args.verbose:
-	print(f'depth: {4 if fourBits else 8}, compressed: {1 if compressed else 0}')
+	print(f'depth: {depth}')
+	print(f'flags: {flags}')
 	print(f'width: {im.width}, height: {im.height}, stride: {stride}')
 	print(f'palLength: {palLength} (true {truePalLength})')
-	print(f'palSize: {palSize}')
-	print(f'palOffset: {0x18}')
-	print(f'dataSize: {len(outData)}')
-	print(f'dataOffset: {0x18 + palSize}')
+	print(f'palSize: 0x{palSize:X}')
+	print(f'palOffset: 0x{palOffset:X}')
+	print(f'dataSize: 0x{len(outData):X}')
+	print(f'dataOffset: 0x{dataOffset:X}')
+	if len(hdmaChannels) > 0:
+		print(f'hdma: {hdmaChannels}')
+		print(f'hdmaOffset: 0x{hdmaOffset:X}')
 
 bf = io.BytesIO()
 bf.write(b'AIMG')
-bf.write(b'\x04' if fourBits else b'\x08')
-bf.write(b'\x01' if compressed else b'\x00')
+bf.write(struct.pack('>b', depth))
+bf.write(struct.pack('>b', flags))
 bf.write(struct.pack('>H', im.width))
 bf.write(struct.pack('>H', im.height))
 bf.write(struct.pack('>H', stride))
-bf.write(struct.pack('>L', len(outData)))
-bf.write(struct.pack('>L', 0x18))
-bf.write(struct.pack('>L', 0x18 + palSize))
+bf.write(struct.pack('>L', im.height * stride)) #len(outData)))
+bf.write(struct.pack('>L', palOffset))
+bf.write(struct.pack('>L', dataOffset))
+if len(hdmaChannels) > 0:
+	bf.write(struct.pack('>L', hdmaOffset))	
 
 for i in range(palLength):
 	if i < truePalLength:
@@ -126,6 +157,53 @@ for i in range(palLength):
 	bf.write(struct.pack('>H', snes))
 
 bf.write(outData)
+
+if len(hdmaChannels) > 0:
+	bf.write(struct.pack('>b', len(hdmaChannels)))
+	for i in hdmaChannels:
+		clipgrads = args.clipgrads
+		im = Image.open(stem + f'-h{i}.png')
+		if im.mode != 'RGB':
+			im = im.convert('RGB')
+		tl = im.getpixel((0,0))
+		tr = im.getpixel((im.size[0]-1,0))
+		if tr == (255, 255, 255) and tl != (255, 255, 255):
+			clipgrads = False
+			if args.verbose:
+				print(f'Disabling clipping for channel {i}.')
+		imgh = im.size[1]
+		gradient = []
+		for y in range(imgh):
+			r, g, b = im.getpixel((0, y))
+			snes = ((b >> 3) << 10) | ((g >> 3) << 5) | (r >> 3)
+			gradient.append(snes)
+		start = 0
+		stop = imgh
+		if clipgrads:
+			first = gradient[start]
+			last = gradient[stop - 1]
+			for y in range(start, stop):
+				if gradient[y] != first:
+					start = y
+					break
+			for y in range(stop - 1, start + 2, -1):
+				if gradient[y] != last:
+					stop = y
+					break
+			gradient = gradient[start:stop]
+		if imgh <= 240:
+			start = start * 2
+			stop = stop * 2
+		length = stop - start
+		hdmaControl = 1 | (1 << 4) | (start << 8) | (length << 20)
+		if imgh == 240:
+			hdmaControl = hdmaControl | 0x80
+		bf.write(struct.pack('>H', len(gradient) * 2))
+		bf.write(struct.pack('>L', hdmaControl))
+		for y in gradient:
+			bf.write(struct.pack('>H', y))
+		if args.verbose:
+			print(f'hdma[{i}]: 0x{hdmaControl:X}, size 0x{len(gradient):X}, {start} to {stop}, for {length}')
 
 if Path(args.outFile).suffix == '.c' or Path(args.outFile).suffix == '.s':
 	asS = not Path(args.outFile).suffix == '.c'
