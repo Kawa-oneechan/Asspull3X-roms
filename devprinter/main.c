c5e58ef20f7785f995852295f0775fb0e7312659
Kawa <kawaoneechan@gmail.com>
2022-10-12 23:38:56 +0200

    DevPrinter: use Interface, add cow

--- devprinter/main.c
+++ devprinter/main.c
@@ -1,28 +1,10 @@
 #include "../ass.h"
 IBios* interface;
 
-//The following, with the printf statements removed, may be moved into the BIOS.
-char* LP_Out = 0;
-void LP_Init()
-{
-	printf("Looking for the line printer... ");
-	LP_Out = 0;
-	for (int i = 0; i < 16; i++)
-	{
-		uint16_t signature = *(uint16_t*)(0x02000000 + (i * 0x8000));
-		if (signature == 0x4C50)
-		{
-			printf("Found as device #%d.\n", i);
-			LP_Out = (char*)(0x02000002 + (i * 0x8000));
-			return;
-		}
-	}
-	printf("Couldn't find it.\n");
-}
 void LP_Print(const char* text)
 {
 	while (*text)
-		*LP_Out = *text++;
+		*interface->LinePrinter = *text++;
 }
 //------
 
@@ -32,8 +14,20 @@ int main(void)
 	MISC->SetTextMode(SMODE_240 | SMODE_BOLD);
 	TEXT->SetTextColor(1, 7);
 	TEXT->ClearScreen();
-	printf("Hello, world!\n");
+	if (interface->LinePrinter == 0)
+	{
+		printf("No line printer attached.");
+		return;
+	}
+	printf("Look at the printer.");
 
-	LP_Init();
 	LP_Print("Hello to the lineprinter!\n");
+	LP_Print("This has some " "\x1B" "Ebold text." "\x1B" "e.\n");
+	LP_Print("This has some " "\x1B" "Uunderlined text." "\x1B" "u.\n");
+	LP_Print("This has " "\x1B" "E" "\x1B" "Uboth at once." "\x1B" "u" "\x1B" "e.\n");
+	LP_Print("        \\   ^__^\n");
+	LP_Print("         \\  (oo)\\_______\n");
+	LP_Print("            (__)\\       )\\/\\\n");
+	LP_Print("                ||----w |\n");
+	LP_Print("                ||     ||\n");
 }
